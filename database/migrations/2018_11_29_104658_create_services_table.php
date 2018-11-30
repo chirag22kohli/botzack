9162cf62d3ab38fd6dbe7b695cbb4f30a96b40ad
unknown <chirag22.kohli@gmail.com>
2018-11-30 14:51:38 +0530

    entities

--- database/migrations/2018_11_29_104658_create_services_table.php
+++ database/migrations/2018_11_29_104658_create_services_table.php
@@ -12,7 +12,7 @@ class CreateServicesTable extends Migration
      */
     public function up()
     {
-        Schema::update('services', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->softDeletes();
